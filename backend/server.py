ff6e1d9c6e007508dc3883899580d23edae180a6
josdas <josdas@mail.ru>
2017-09-24 12:10:18 +0300

    bug fixed

--- backend/server.py
+++ backend/server.py
@@ -12,10 +12,6 @@ import random
 import string
 import copy
 
-UPLOAD_FOLDER = '/tmp'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-
 class Server(Resource):
     """
     Special class for flask
@@ -111,45 +107,10 @@ class Server(Resource):
             return SPECIAL_ANSWER
         return SPECIAL_ANSWER
 
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
 def start_server():
     app = Flask(__name__, static_url_path='/image', static_folder='tmp')
     api = Api(app)
     api.add_resource(Server, '/<string:function_name>/<args>')
-
-    @app.route('/', methods=['GET', 'POST'])
-    def upload_file():
-        if request.method == 'POST':
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            # if user does not select file, browser also
-            # submit a empty part without filename
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, filename))
-                return redirect(url_for('uploaded_file',
-                                        filename=filename))
-        return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-          <p><input type=file name=file>
-             <input type=submit value=Upload>
-        </form>
-        '''
-
     app.run()
 
 
